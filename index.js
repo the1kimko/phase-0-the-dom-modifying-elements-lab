03f80a01bed3fed3586c6d8657373fb904fae277
KomfortKimko <komfortkimk@gmail.com>
2024-07-05 11:40:19 +0300

    DOM-modifying-elements

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+document.querySelector('main#main').remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML = 'KIMKO is the champion';
+
+document.body.appendChild(newHeader);
